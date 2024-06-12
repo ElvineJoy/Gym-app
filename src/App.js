e111c1fc3bdbe6b651687d931d9b2dfcd7edc7ba
ElvineJoy <elvinkuiyungest@gmail.com>
2024-06-12 18:36:07 +0300

    Installling bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Home from './Components/Home';
 import Workouts from './Components/Workouts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
